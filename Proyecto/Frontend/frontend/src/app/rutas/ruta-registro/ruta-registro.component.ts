601292ef8e6c27bb84f598f99f25c74282382ffe
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-09-24 01:44:53 -0500

    registro de usuarios para luego logearse

--- Proyecto/Frontend/frontend/src/app/rutas/ruta-registro/ruta-registro.component.ts
+++ Proyecto/Frontend/frontend/src/app/rutas/ruta-registro/ruta-registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UsuarioService} from "../../servicios/http/usuario.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-ruta-registro',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaRegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly _usuarioService:UsuarioService,
+    private readonly _router:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  crearUsuario(usuario){
+    const obsCrearUsuario = this._usuarioService.crearUsuario(usuario);
+    obsCrearUsuario
+      .subscribe(
+        (datos:Object) => {
+          //   alert('Nuevo usuario creado');
+          console.log('Nuevo Usuario', datos);
+          const url = ['/login']
+          this._router.navigate(url);
+        },
+        (error) => {
+          console.error('Error', error);
+        }
+      )
   }
 
 }
